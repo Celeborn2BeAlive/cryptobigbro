fb9ca297550964b1a78edfe8b1ac19ba490f3148
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2019-05-04 00:18:12 +0200

    Change systemd scripts

--- systemd/cryptobigbro-update.sh
+++ systemd/cryptobigbro-update.sh
@@ -1,8 +1,8 @@
 EXCHANGE=$1
 cd $CRYPTOBIGBRO_PATH_TO_DATA/$EXCHANGE
-# git pull # pull existing data
+git pull # pull existing data
 source $CRYPTOBIGBRO_PATH_TO_CODE/venv/bin/activate
 python $CRYPTOBIGBRO_PATH_TO_CODE/cryptobigbro.py fetch-ohlcv $EXCHANGE .
-# git add *
-# git commit -a -m "Update OHLCV data"
-# git push
+git add *
+git commit -a -m "Update OHLCV data"
+git push
