a7eff6ae8b88d6b22470aa8c9e911a645de0b9c6
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2019-05-04 01:04:21 +0200

    Iterate over instruments for update instead of processing eveything in one call (to reduce impact of errorsÃ)

--- systemd/cryptobigbro-update.sh
+++ systemd/cryptobigbro-update.sh
@@ -2,7 +2,11 @@ EXCHANGE=$1
 cd $CRYPTOBIGBRO_PATH_TO_DATA/$EXCHANGE
 git pull # pull existing data
 source $CRYPTOBIGBRO_PATH_TO_CODE/venv/bin/activate
-python $CRYPTOBIGBRO_PATH_TO_CODE/cryptobigbro.py fetch-ohlcv $EXCHANGE .
-git add *
-git commit -a -m "Update OHLCV data"
+instruments=`python $CRYPTOBIGBRO_PATH_TO_CODE/cryptobigbro.py list-instruments $EXCHANGE`
+for i in ${instruments//,/ }
+do
+	python $CRYPTOBIGBRO_PATH_TO_CODE/cryptobigbro.py fetch-ohlcv $EXCHANGE . --instruments ${i}
+	git add *
+	git commit -a -m "Update OHLCV data for exchange ${EXCHANGE} and instrument ${i}"
+done
 git push
