3c45dcac54d418d866cf7f7ad16c0ca9cd972f2c
Celeborn2BeAlive <laurent.noel.c2ba@gmail.com>
2019-08-12 14:09:51 +0200

    Add bokeh library and a script to plot ohlcv data

---
+++ plot-ohlcv.py
@@ -0,0 +1,43 @@
+import argparse
+import pandas as pd
+from datetime import datetime, timezone, timedelta
+from bokeh.plotting import figure, show, output_file
+
+def parse_cli_args():
+    parser = argparse.ArgumentParser(description='Plot an input csv file containing OHLCV data to an html document.')
+
+    parser.add_argument("csv_file", type=str, help="Path to CSV file to plot.")
+    parser.add_argument("-o", "--output-file", type=str, help="Path to output html file.")
+
+    return parser.parse_args()
+
+def main():
+    args = parse_cli_args()
+
+    df = pd.read_csv(args.csv_file)
+    df["date"] = df["open_timestamp_utc"].map(lambda x: datetime.fromtimestamp(x))
+    
+    w = (df["open_timestamp_utc"][1] - df["open_timestamp_utc"][0]) * 1000 * 0.5
+
+    inc = df.close > df.open
+    dec = df.open > df.close
+
+    TOOLS = "pan,wheel_zoom,box_zoom,reset,save,crosshair"
+
+    p = figure(x_axis_type="datetime", tools=TOOLS, plot_width=1000, title = "OHLCV")
+
+    p.grid.grid_line_alpha=0.3
+
+    p.segment(df.date, df.high, df.date, df.low, color="black")
+    p.vbar(df.date[inc], w, df.open[inc], df.close[inc], fill_color="#D5E1DD", line_color="black")
+    p.vbar(df.date[dec], w, df.open[dec], df.close[dec], fill_color="#F2583E", line_color="black")
+
+    if args.output_file:
+        output_file(args.output_file, title="OHLCV")
+
+    show(p)
+
+    return
+
+if __name__ == "__main__":
+    main()
